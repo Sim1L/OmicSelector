9e0197d177810a685fd1af03672bc3628ab84d50
Sim1L <simone.lucchesi2@unisi.it>
2024-02-15 16:47:44 +0100

    corretta funzione OmicSelector_create_formula

--- R/OmicSelector_create_formula.R
+++ R/OmicSelector_create_formula.R
@@ -9,6 +9,7 @@
 OmicSelector_create_formula = function(selected_features) {
   if (sum(selected_features == ".")>0 | length(selected_features) == 0) {
     stop("Too few features selected.")
+    }
   selected_features<-selected_features[!is.na(selected_features)]
   as.formula(paste0("Class ~ ",paste0(as.character(selected_features), collapse = " + ")))
 }
